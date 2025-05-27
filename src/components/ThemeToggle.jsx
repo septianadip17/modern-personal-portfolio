f9feaf016769a5a05b90b4a97872f6b8bfef74c6
Septian Adi Pratama <septianadip7998@gmail.com>
2025-05-27 20:54:21 +0700

    color changed and toggle showed

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -6,5 +6,5 @@ import { Moon, Sun } from 'lucide-react'
 export const ThemeToggle = () => {
   const [isDarkMode, setIsDarkMode] = useState(false)
 
-  return <button>{isDarkMode ? <Sun/>: <Moon/>}</button>
+  return <button>{isDarkMode ? <Sun className='h-6 w-6 text-yellow-300'/>: <Moon className='h-6 w-6 text-blue-900'/>}</button>
 }
