8a7a989fd8f5ce64f78e2c22fada0f4606a61073
Septian Adi Pratama <septianadip7998@gmail.com>
2025-05-28 18:41:25 +0700

    utils added

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,10 +1,39 @@
 /* eslint-disable no-unused-vars */
-import { useState } from 'react';
-import { Moon, Sun } from 'lucide-react'
-
+import { useEffect, useState } from "react";
+import { Moon, Sun } from "lucide-react";
 
 export const ThemeToggle = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false)
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
-  return <button>{isDarkMode ? <Sun className='h-6 w-6 text-yellow-300'/>: <Moon className='h-6 w-6 text-blue-900'/>}</button>
-}
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("theme");
+    if (storedTheme === "dark") {
+      document.documentElement.classList.add("dark");
+      setIsDarkMode(true);
+    } else {
+      setIsDarkMode(false);
+      localStorage.setItem("theme", "light"); 
+    } 
+  });
+
+  const toggleTheme = () => {
+    if (isDarkMode) {
+      document.documentElement.classList.remove("dark");
+      setIsDarkMode(false);
+      localStorage.setItem("theme", "light");
+    } else {
+      document.documentElement.classList.add("dark");
+      setIsDarkMode(true);
+      localStorage.setItem("theme", "dark");
+    }
+  };
+  return (
+    <button onClick={toggleTheme}>
+      {isDarkMode ? (
+        <Sun className="h-6 w-6 text-yellow-300" />
+      ) : (
+        <Moon className="h-6 w-6 text-blue-900" />
+      )}
+    </button>
+  );
+};
