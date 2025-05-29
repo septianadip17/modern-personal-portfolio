51d327326dcc9bd4f41eb10baa42fa766ed10e95
Septian Adi Pratama <septianadip7998@gmail.com>
2025-05-29 21:43:35 +0700

    hero comment added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ export const Home = () => {
   return <div className="min-h-screen bg-background text-foreground overflow-x-hidden ">
     {/* Theme Toggle */}
     <ThemeToggle/>
+    {/* Hero Section */}
 
     {/* Background Effects */}
 
