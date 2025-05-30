7ceadfac6b16f47d7c99686cfbd220f8fd9552d4
Septian Adi Pratama <septianadip7998@gmail.com>
2025-05-30 22:14:22 +0700

    theme showd

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,10 @@ export const Home = () => {
   return <div className="min-h-screen bg-background text-foreground overflow-x-hidden ">
     {/* Theme Toggle */}
     <ThemeToggle/>
+    console.log(ThemeToggle)
+
     {/* Hero Section */}
+
 
     {/* Background Effects */}
 
