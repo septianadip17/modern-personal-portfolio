5285d407a422c850d049e5a9b199d66256bad209
Septian Adi Pratama <septianadip7998@gmail.com>
2025-06-01 22:55:42 +0700

    delete console log

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,6 @@ export const Home = () => {
   return <div className="min-h-screen bg-background text-foreground overflow-x-hidden ">
     {/* Theme Toggle */}
     <ThemeToggle/>
-    console.log(ThemeToggle)
-
     {/* Hero Section */}
 
 
