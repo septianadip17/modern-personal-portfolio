788614e93d4cfbc7a22f5ccf80b302195d23c1be
Septian Adi Pratama <septianadip7998@gmail.com>
2025-06-06 23:15:58 +0700

    not found setting

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,12 +1,30 @@
 export const NotFound = () => {
-  return <h1>Not Found</h1>;
+  return (
+    <div>
+      <h1>404 - Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+      <p>Please check the URL or return to the homepage.</p>
+      <a href="/">Go to Homepage</a>
+      <style jsx>{`
+        div {
+          text-align: center;
+          margin-top: 50px;
+        }
+        h1 {
+          font-size: 2em;
+          color: #ff0000;
+        }
+        p {
+          font-size: 1.2em;
+        }
+        a {
+          color: #007bff;
+          text-decoration: none;
+        }
+        a:hover {
+          text-decoration: underline;
+        }
+      `}</style>
+    </div>
+  );
 };
-
-
-
-
-
-
-
-
-
