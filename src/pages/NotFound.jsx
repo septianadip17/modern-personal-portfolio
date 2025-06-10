87ec963a343a3915cbb505de72d3d34327f1d93e
Septian Adi Pratama <septianadip7998@gmail.com>
2025-06-10 23:32:32 +0700

    info added

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -5,6 +5,8 @@ export const NotFound = () => {
         <h1 className="">404 - Not Found</h1>
         <p>The page you are looking for does not exist.</p>
         <p>Please check the URL or return to the homepage.</p>
+        <p>If you believe this is an error, please contact support.</p>
+        <p>Thank you for your understanding!</p>
       </div>
 
       <a href="/">Go to Homepage</a>
